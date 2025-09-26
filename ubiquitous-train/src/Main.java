9472d9ee0c5675872c0a34feabc2e3f6ea12e7e9
Nvoth8 <noahjvoth@gmail.com>
2025-09-26 09:56:33 -0500

    Noah changed it try 2

--- ubiquitous-train/src/Main.java
+++ ubiquitous-train/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("bob eats human flesh");
 		System.out.println("no, he eats cookies that take a day to make");
+		
 	}
 
 }
