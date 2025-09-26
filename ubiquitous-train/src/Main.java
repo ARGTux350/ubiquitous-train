d1192e7a24235640dbbeff49ae244f74217b3895
Nvoth8 <noahjvoth@gmail.com>
2025-09-26 09:51:33 -0500

    Noah changed it

--- ubiquitous-train/src/Main.java
+++ ubiquitous-train/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("bob eats human flesh");
+		System.out.println("no, he eats cookies that take a day to make");
 	}
 
 }
